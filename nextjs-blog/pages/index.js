61427fb45e8c914d7621df43f805a8a0567ba071
Vicki Chen <missvickies14@gmail.com>
2021-08-23 22:27:52 -0400

    added dependencies

---
+++ nextjs-blog/pages/index.js
@@ -0,0 +1,13 @@
+import Head from "next/head";
+
+export default function Home() {
+  return (
+    <div className="container">
+      <head>
+        <title> vicki Chen Portfolio</title>
+      </head>
+
+      <main></main>
+    </div>
+  );
+}
